6370982f21341b8605ea3ccab43948b0783d1d55
Aiday <aidai.totoeva@gmail.com>
2022-11-24 10:09:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// TABS
+const tabsBtns = document.querySelectorAll(".tabs__nav button");
+const tabsItems = document.querySelectorAll(".tabs__item");
+
+// Ф-я скрывает табы и убирает active у кнопок
+function hideTabs() {
+  tabsItems.forEach(item => item.classList.add("hide"));
+  tabsBtns.forEach(item => item.classList.remove("active"));
+}
+
+// Ф-я показывает переданный номер таба и делает соответствующую ему кнопку активной.
+function showTab(index) {
+  tabsItems[index].classList.remove("hide");
+  tabsBtns[index].classList.add("active");
+}
+
+hideTabs();
+showTab(0);
+
+
+tabsBtns.forEach((btn, index) => btn.addEventListener("click", () => {
+  hideTabs();
+  showTab(index);
+}));
+
+
+
+
+// Anchors
+const anchors = document.querySelectorAll(".header__nav a");
+
+anchors.forEach(anc => {
+  anc.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const id = anc.getAttribute("href");
+    const elem = document.querySelector(id);
+
+
+    window.scroll({
+      top: elem.offsetTop - 80,
+      behavior: 'smooth'
+    });
+  });
+});
